e6f8aa6448204dc713e959fb8cf3a98b9c0eb418
Jhon <jhon.durango18@gmail.com>
2025-09-29 16:51:01 -0500

    feat: add About Me section (#2)

---
+++ src/app/components/aboutMe-section/aboutMe-section.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'about-me-section',
+  imports: [],
+  templateUrl: './aboutMe-section.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AboutMeSectionComponent { }
